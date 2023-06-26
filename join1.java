2a8449ab7ac6e1340e76995046a55ea15fd70772
anirudhkaluri <anirudhkaluri@gmail.com>
2023-06-26 17:38:57 +0530

    modified join1 exception handling for a more easily readable code

--- join1.java
+++ join1.java
@@ -1,24 +1,22 @@
 public class join1 {
 
-    public static void main(String[]args){
+    public static void main(String[]args) throws Exception{
         threadClass t1=new threadClass();
-        System.out.println("We careated the thread object. going to call the start method");
+
         t1.start();
-        System.out.println("Called the Start method");
-        try {
-            t1.join();
-        }
-        catch(Exception e){
-            System.out.println("Exception caught");
-        }
-        System.out.println("Thread execution completed. Print statement after join");
+        System.out.println("Called the Start method.calling join now.");
+
+        t1.join();
+
+
+        System.out.println("\nThread execution completed. Print statement after join");
 
     }
 }
 
 class threadClass extends Thread{
     public void run(){
-        for(int i=0;i<10000;i++)
+        for(int i=0;i<1000;i++)
             System.out.print(i+",");
     }
 }
